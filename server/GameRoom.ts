6b88e8a56fa1773fd5d09ab0000a89127f92664f
Matt Del Signore <mattdelsig@me.com>
2020-06-21 15:21:05 -0400

    Added state for the players

--- server/GameRoom.ts
+++ server/GameRoom.ts
@@ -1,7 +1,25 @@
 import { Room, Client } from "colyseus";
+import { Schema, type, MapSchema } from "@colyseus/schema";
 
-export class GameRoom extends Room {
+export class Player extends Schema {
+  @type("string")
+  name: String;
+}
+
+export class WordGameState extends Schema {
+  @type({ map: Player })
+  players = new MapSchema<Player>();
+
+  //Id of the current player who's turn is it
+  @type("string")
+  currentTurn: string;
+}
+
+export class GameRoom extends Room<WordGameState> {
   onCreate(options: any) {
+    //Set the intial State
+    this.setState(new WordGameState());
+
     this.onMessage("type", (client, message) => {
       // handle "type" message
     });
