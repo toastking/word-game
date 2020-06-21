85276488b2d37c1c319677dcab3e2d8bd21f13ef
Matt Del Signore <mattdelsig@me.com>
2020-06-21 16:40:35 -0400

    Removed uneeded components and added buefy

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Colyseus from "colyseus.js";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(Buefy);
+
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
+
+// Initialize the game client
+/** Game server client */
+export const client = new Colyseus.Client("ws://localhost:2567");
