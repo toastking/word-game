5c0d14ea642a8dfc7103f2b34f58762f75cc3639
Matt Del Signore <mattdelsig@me.com>
2020-07-02 11:29:04 -0400

    Changed property files so work with typescript compilers

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
   },
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
+  },
+
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier/@typescript-eslint",
+    "@vue/typescript",
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
 };
